96d39b7fe161b9c3e694f6c20cef6adf79f3d549
Tejas Raman <134980520+Traman2@users.noreply.github.com>
2026-08-04 15:07:41 -0500

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int l = 0;
+        int r = 1;
+        int maxP = 0;
+
+        while(r < prices.length){
+            if(prices[l] > prices[r]) l = r;
+            else maxP = Math.max(maxP, prices[r] - prices[l]);
+            r++;
+        }
+
+        return maxP;
+    }
+}
